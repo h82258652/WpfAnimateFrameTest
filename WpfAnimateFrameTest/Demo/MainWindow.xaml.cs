068891d34a43663bcab0ba4ad4945b45f7865be2
h82258652 <h82258652@hotmail.com>
2016-12-03 10:25:13 +0800

    working on it.

--- WpfAnimateFrameTest/Demo/MainWindow.xaml.cs
+++ WpfAnimateFrameTest/Demo/MainWindow.xaml.cs
@@ -12,8 +12,6 @@ namespace Demo
         private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
         {
             Frame.Navigate(typeof(TestPage1));
-
-            System.Windows.Controls.Frame
         }
     }
 }
